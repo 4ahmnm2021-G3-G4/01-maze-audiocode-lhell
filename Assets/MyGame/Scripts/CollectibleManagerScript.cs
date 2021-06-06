a32c0eb3a020aabe2ae9b950e620e1250986025e
HellLisa <hell-lm@gmx.at>
2021-06-06 19:18:41 +0200

    tried to do collision, but failed terribly

---
+++ Assets/MyGame/Scripts/CollectibleManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollectibleManagerScript : MonoBehaviour
+{
+
+    public bool collectibleGrapes;
+    public bool collectibleApple;
+    public bool collectibleCherries;
+       
+
+    void Update()
+    {
+
+        if (collectibleGrapes == true && collectibleApple == true && collectibleCherries == true) 
+        
+        {
+        
+        Debug.Log ("Alles eingesammelt");
+        //open the second gate
+        
+        }
+        
+    }
+}
