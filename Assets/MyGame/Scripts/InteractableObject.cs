725298d6812ca8031edaca5921e4f3e9b650e874
HellLisa <hell-lm@gmx.at>
2021-06-14 08:50:59 +0200

    add Interactbale Objects script from p&c

---
+++ Assets/MyGame/Scripts/InteractableObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class InteractableObject : MonoBehaviour
+{
+    public bool condition;
+    public bool condition2;
+    public bool condition3;
+    
+    public UnityEvent conditionMet;
+    public UnityEvent conditionNotMet;
+    public UnityEvent bothConditionsMet;
+    public UnityEvent allThreeConditionsMet;
+
+    public void Interact()
+    {
+        if (condition)
+        {
+            conditionMet.Invoke();
+        }
+        else 
+        {
+            conditionNotMet.Invoke();
+        }
+
+        if(condition2 && condition)
+        {
+            bothConditionsMet.Invoke();
+        }
+
+        if(condition2 && condition && condition3)
+        {
+            allThreeConditionsMet.Invoke();
+        }
+    }
+
+     public void SetCondition(bool value)
+        {
+            condition = value;
+        }
+    public void SetCondition2(bool value)
+        {
+            condition2 = value;
+        }
+    public void SetCondition3(bool value)
+        {
+            condition3 = value;
+        }
+}
