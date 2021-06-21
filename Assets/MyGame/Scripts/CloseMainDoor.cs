6bd687a845f6680c25687e939fa88d7ec5c3bd56
HellLisa <hell-lm@gmx.at>
2021-06-21 08:20:46 +0200

    add Decoration and Start door closes now

---
+++ Assets/MyGame/Scripts/CloseMainDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseMainDoor : MonoBehaviour
+{
+
+    public Animator animator;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Detect collision");
+
+        animator.SetBool("collision", true);
+    }
+}
