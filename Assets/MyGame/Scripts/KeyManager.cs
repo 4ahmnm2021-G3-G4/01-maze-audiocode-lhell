da29ad4f5e407a9c855df83b17f075053e0868f3
HellLisa <hell-lm@gmx.at>
2021-06-27 23:56:03 +0200

    add key script to open the last gate

---
+++ Assets/MyGame/Scripts/KeyManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManager : MonoBehaviour
+{
+    public GameObject key;
+    public Animator animator;
+
+    private void OnTriggerEnter(Collider key)
+    {
+
+        Debug.Log("key to door works");
+
+        Destroy(key.gameObject);
+
+        animator.SetBool("key", true);
+    }
+}
